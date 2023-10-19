f857f66a231c2ba17ec5559b5747d3c6e9db3046
jabber <2898117012@qq.com>
2023-10-19 20:03:12 +0800

    init

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+}
---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+let mix = require('laravel-mix');
+
+mix.js('src/assets/js/app.js', 'assets/js').sass('src/assets/scss/style.scss', '', [], [require('postcss-import'), require('autoprefixer'),]).sass('src/assets/scss/style-rtl.scss', '', [], [require('postcss-import'), require('rtlcss'), require('autoprefixer'),]);
