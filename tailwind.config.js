97ce202b35d409645f4c2469811d93ff90f4d564
jabber <2898117012@qq.com>
2023-10-29 01:40:24 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {},
+    },
+    corePlugins: {
+        preflight: false,
+    },
+    experimental: {
+        optimizeUniversalDefaults: true,
+    },
+    plugins: [],
+}
+
